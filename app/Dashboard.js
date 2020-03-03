76110b87ad658e06622e43236fc58da118d500b8
wkibbler <william.kibbler@googlemail.com>
2020-03-03 09:17:53 +0000

    icons

--- app/Dashboard.js
+++ app/Dashboard.js
@@ -10,18 +10,18 @@ export default class Dashboard extends Component {
     return (
         <View style={styles.background}>
           <TouchableOpacity style={[styles.coinWrapper, {marginTop: 70}]}>
-          <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['rgb(233, 122, 22)', 'rgb(247, 156, 74)']} style={{height: 110, width: width - 70, borderRadius: 10}}>
-
+          <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['rgb(233, 122, 22)', 'rgb(247, 156, 74)']} style={styles.card}>
+            <Image style={styles.icon} source={require('../assets/bitcoin.png')}/>
           </LinearGradient>
           </TouchableOpacity>
           <TouchableOpacity style={styles.coinWrapper}>
-          <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['rgb(130, 130, 130)', 'rgb(223, 223, 223)']} style={{height: 110, width: width - 70, borderRadius: 10}}>
-
+          <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['rgb(130, 130, 130)', 'rgb(223, 223, 223)']} style={styles.card}>
+            <Image style={styles.icon} source={require('../assets/litecoin.png')}/>
           </LinearGradient>
           </TouchableOpacity>
           <TouchableOpacity style={styles.coinWrapper}>
-          <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['rgb(19, 64, 115)', 'rgb(23, 142, 159)']} style={{height: 110, width: width - 70, borderRadius: 10}}>
-
+          <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['rgb(19, 64, 115)', 'rgb(23, 142, 159)']} style={styles.card}>
+            <Image style={styles.icon} source={require('../assets/ilcoin.png')}/>
           </LinearGradient>
           </TouchableOpacity>
         </View>
@@ -47,5 +47,16 @@ const styles = StyleSheet.create({
       width: width - 700,
       height: 110,
       marginTop: 30
+    },
+    card: {
+      height: 110, 
+      width: width - 70, 
+      borderRadius: 10,
+      justifyContent: 'center'
+    },
+    icon: {
+      width: 60,
+      height: 60,
+      marginLeft: 20
     }
 })
